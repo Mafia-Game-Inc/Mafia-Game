53caaebf74ae6289b483db6f3f223869b2230229
momopoi443 <myzika99993824@gmail.com>
2022-09-18 15:29:00 +0300

    sketch

---
+++ Main.kt
@@ -0,0 +1,22 @@
+class Main {
+    fun main() {
+        while (Game.checkRuls()) {
+            night()
+            Game.day() {
+                for (player in Game.players) {
+                    plaeyr.say(60)
+                    asdf
+                }
+                for (player in Game.выставленые) {
+                    plaeyr.say(30)
+                }
+
+                for (player in Game.players) {
+                    plaeyr.vote()
+                }
+
+                Game.makeConslusion()
+            }
+        }
+    }
+}
