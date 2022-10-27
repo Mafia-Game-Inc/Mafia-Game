7e4d2268ac0b107794759fc0cfd1242224c56f5f
momopoi443 <myzika99993824@gmail.com>
2022-10-27 13:01:04 +0300

    refactored Sasha`s_R code

--- app/src/main/kotlin/game/mafia/roles/Classic.kt
+++ app/src/main/kotlin/game/mafia/roles/Classic.kt
@@ -1,6 +1,7 @@
 package game.mafia.roles
 
 import game.mafia.users.*
+import java.util.*
 
 class ClassicPreSet: PreSet {
     val activeRolesAmount = 3
@@ -8,7 +9,40 @@ class ClassicPreSet: PreSet {
 
     override fun configure(players: Int) {}
 
-    override fun runNight(players: Array<Player>) {}
+    override fun runNight(players: Array<Player>) {
+        for (player in players) {
+            if (player.role == Roles.SHERIFF && player.state == UserState.ALIVE) {
+                println("Hey, Sheriff, try to find mafia")
+
+                val reader = Scanner(System.`in`)
+                val integer = reader.nextInt()
+
+                //Check for validity of input (integer >= 1)
+                checkForMafia(players[integer - 1])
+            }
+        }
+
+        for (player in players) {
+            if (player.role == Roles.GODFATHER && player.state == UserState.ALIVE) {
+                println("Hey, Don, try to find Sheriff")
+
+                val reader = Scanner(System.`in`)
+                val integer = reader.nextInt()
+
+                //Check for validity of input (integer >= 1)
+                checkForCom(players[integer-1])
+            }
+        }
+
+        println("Now, choose to kill!")
+
+        val reader = Scanner(System.`in`)
+        val integer = reader.nextInt()
+
+        kill(players[integer - 1])
+        var position = integer - 1
+        println("Player $position was killed")
+    }
 }
 
 fun kill(player: Player) {
--- app/src/main/kotlin/game/mafia/roles/PreSet.kt
+++ app/src/main/kotlin/game/mafia/roles/PreSet.kt
@@ -2,36 +2,9 @@ package game.mafia.roles
 
 import game.mafia.users.*
 
-import java.util.Scanner
-
 interface PreSet {
 
     fun configure(players: Int)
 
-    fun runNight(players: Array<Player>) {
-        for (player in players) {
-            if (player.role == Roles.SHERIFF && player.state == UserState.ALIVE) {
-                println("Hey, Sheriff, try to find mafia")
-                val reader = Scanner(System.`in`)
-                var integer:Int = reader.nextInt()
-                //Check for validity of input (integer >= 1)
-                checkForMafia(players[integer - 1])
-            }
-        }
-        for (player in players) {
-            if (player.role == Roles.GODFATHER && player.state == UserState.ALIVE) {
-                println("Hey, Don, try to find Sheriff")
-                val reader = Scanner(System.`in`)
-                var integer:Int = reader.nextInt()
-                //Check for validity of input (integer >= 1)
-                checkForCom(players[integer-1])
-            }
-        }
-        println("Now, choose to kill!")
-        val reader = Scanner(System.`in`)
-        var integer:Int = reader.nextInt()
-        kill(players[integer - 1])
-        var position = integer - 1
-        println("Player $position was killed")
-    }
+    fun runNight(players: Array<Player>)
 }
