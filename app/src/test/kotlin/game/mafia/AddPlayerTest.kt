62ff1c0a02e5a970b21feae072e24c5f7b307c63
lilarin <80650911+lilarin@users.noreply.github.com>
2022-11-19 22:02:57 +0200

    Test

--- app/src/test/kotlin/game/mafia/AddPlayerTest.kt
+++ app/src/test/kotlin/game/mafia/AddPlayerTest.kt
@@ -10,6 +10,18 @@ internal class AddPlayerTest {
 
     @Test
     fun addPlayer() {
+        val player = Player("testExample")
+        this.players.size = 10
+
+        mafia.addPlayer(player)
+
+        assertTrue(player.state != UserState.SPECTATOR,
+            () → "Player state incorrect assigned"
+        )
+
+        assertNull(
+            player.position, "Player position incorrect assigned"
+        )
 
     }
 }
