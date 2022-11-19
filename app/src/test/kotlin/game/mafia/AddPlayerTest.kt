b032038afe36f5a4553364f78c51c21a6c451f66
lilarin <80650911+lilarin@users.noreply.github.com>
2022-11-19 21:41:26 +0200

    Test

---
+++ app/src/test/kotlin/game/mafia/AddPlayerTest.kt
@@ -0,0 +1,15 @@
+package game.mafia
+
+import game.mafia.roles.Roles
+import game.mafia.users.Player
+import game.mafia.users.UserState
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class AddPlayerTest {
+
+    @Test
+    fun addPlayer() {
+
+    }
+}
