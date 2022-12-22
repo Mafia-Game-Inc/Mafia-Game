7ad896d555245d65bcc019cce65b15bdae71d8ab
momopoi443 <myzika99993824@gmail.com>
2022-12-22 16:26:57 +0200

    added prototypes of view methods

--- app/src/main/kotlin/game/view/View.kt
+++ app/src/main/kotlin/game/view/View.kt
@@ -1,4 +1,45 @@
 package game.view
 
-class View {
+import kotlinx.coroutines.processNextEventInCurrentThread
+
+/*
+* 1. how to limit time for console input
+*/
+
+fun log(message: String) {
+    println(message)
+}
+
+fun sendMessage(message: String) {
+    println(message)
+}
+
+fun readBoolean(): Boolean {
+    var input = readln()
+
+    while (input != "false" && input != "true") {
+        sendMessage("the value should be 'false' or 'true', try again:")
+        input = readln()
+    }
+
+    return input.toBoolean()
+}
+
+fun readInt(permissibleValues: List<Int>): Int {
+    var input: Int
+    try {
+        input = readln().toInt()
+    } catch (e: NumberFormatException) {
+        //?
+    }
+
+    while (!permissibleValues.contains(input)) {
+        sendMessage("the value should one of given below")
+        sendMessage("$permissibleValues")
+        sendMessage("try again:")
+        //здесь тоже словить ошибку
+        input = readln().toInt()
+    }
+
+    return input
 }
