e1d27659ebed80fb57b95dc8d931169e0168206c
lilarin <80650911+lilarin@users.noreply.github.com>
2023-01-04 15:56:15 +0200

    The first iteration of the view

--- app/src/main/kotlin/game/view/View.kt
+++ app/src/main/kotlin/game/view/View.kt
@@ -1,45 +1,83 @@
 package game.view
 
 import kotlinx.coroutines.processNextEventInCurrentThread
+import java.util.concurrent.TimeoutException
 
 /*
 * 1. how to limit time for console input
 */
+val MAX_INPUT_LENGTH = 100;
+const val FORBIDDEN_CHARACTERS = "!@#$%^&*()"
 
 fun log(message: String) {
-    println(message)
+    val currentTime = System.currentTimeMillis()
+    val logMessage = "[$currentTime] $message"
+    // Здесь можете разместить код, который будет записывать logMessage в лог-файл или отправлять его в систему мониторинга
+    println(logMessage)
 }
 
-fun sendMessage(message: String) {
-    println(message)
-}
-
-fun readBoolean(): Boolean {
-    var input = readln()
-
-    while (input != "false" && input != "true") {
-        sendMessage("the value should be 'false' or 'true', try again:")
-        input = readln()
+fun processInput(inputString: String, timeRemaining: Long): String {
+    if (timeRemaining <= 0) {
+        throw TimeoutException("Time limit reached")
     }
 
-    return input.toBoolean()
+    if (inputString.length > MAX_INPUT_LENGTH) {
+        throw IllegalArgumentException("Input exceeded maximum length")
+    }
+
+    if (inputString.contains(FORBIDDEN_CHARACTERS)) {
+        throw IllegalArgumentException("Input contains forbidden characters")
+    }
+
+    return inputString
 }
 
-fun readInt(permissibleValues: List<Int>): Int {
-    var input: Int
+fun processInputWithErrorHandling(inputString: String, timeRemaining: Long, errorHandler: (String) -> Unit): String {
     try {
-        input = readln().toInt()
-    } catch (e: NumberFormatException) {
-        //?
+        val processedInput = processInput(inputString, timeRemaining)
+        log("Input processed successfully: $processedInput")
+        return processedInput
+    } catch (e: TimeoutException) {
+        errorHandler("TimeoutException: ${e.message}")
+        log("TimeoutException occurred: ${e.message}")
+    } catch (e: IllegalArgumentException) {
+        errorHandler("IllegalArgumentException: ${e.message}")
+        log("IllegalArgumentException occurred: ${e.message}")
     }
-
-    while (!permissibleValues.contains(input)) {
-        sendMessage("the value should one of given below")
-        sendMessage("$permissibleValues")
-        sendMessage("try again:")
-        //здесь тоже словить ошибку
-        input = readln().toInt()
-    }
-
-    return input
+    return ""
 }
+
+//
+//fun sendMessage(message: String) {
+//    println(message)
+//}
+//
+//fun readBoolean(): Boolean {
+//    var input = readln()
+//
+//    while (input != "false" && input != "true") {
+//        sendMessage("the value should be 'false' or 'true', try again:")
+//        input = readln()
+//    }
+//
+//    return input.toBoolean()
+//}
+
+//fun readInt(permissibleValues: List<Int>): Int {
+//    var input: Int
+//    try {
+//        input = readln().toInt()
+//    } catch (e: NumberFormatException) {
+//        //?
+//    }
+//
+//    while (!permissibleValues.contains(input)) {
+//        sendMessage("the value should one of given below")
+//        sendMessage("$permissibleValues")
+//        sendMessage("try again:")
+//        //здесь тоже словить ошибку
+//        input = readln().toInt()
+//    }
+//
+//    return input
+//}
