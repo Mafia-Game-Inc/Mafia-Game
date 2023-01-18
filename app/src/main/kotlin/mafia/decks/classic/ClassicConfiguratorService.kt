ec87c5e26a5de704c47b60669fda2db3459ddbc0
momopoi443 <myzika99993824@gmail.com>
2023-01-18 04:44:35 +0200

    implemented ClassicConfiguratorService

--- app/src/main/kotlin/mafia/decks/classic/ClassicConfiguratorService.kt
+++ app/src/main/kotlin/mafia/decks/classic/ClassicConfiguratorService.kt
@@ -1,9 +1,40 @@
 package mafia.decks.classic
 
+import exceptions.InvalidStateException
 import mafia.ConfiguratorService
+import mafia.decks.enams.*
+import mafia.models.*
 
 class ClassicConfiguratorService: ConfiguratorService {
-    override fun configure() {
+    private val coefForCalcBlackPlayers = 3
+    private val minPlayersAmount = 10
+    private val minActiveRolesAmount = 4
+    private var activeRoles = listOf(
+        RoleData(2, Teams.BLACK, Roles.MAFIA),
+        RoleData(1, Teams.BLACK, Roles.GODFATHER),
+        RoleData(1, Teams.RED, Roles.SHERIFF)
+    )
 
+    override fun configure() {
+        val playersAmount = Lobby.players.size
+
+        if (playersAmount < minPlayersAmount) {
+            throw InvalidStateException("Invalid game state: there is not enough players in lobby")
+        }
+
+        DeckSettings.playersAmount = playersAmount
+        DeckSettings.blackPlayers =
+            playersAmount / coefForCalcBlackPlayers
+        DeckSettings.redPlayers =
+            playersAmount - DeckSettings.blackPlayers
+        // 1 represents sheriff
+        DeckSettings.activePlayersAmount =
+            1 + DeckSettings.blackPlayers
+
+        val shortageOfMafias =
+            DeckSettings.activePlayersAmount - minActiveRolesAmount
+        activeRoles[0].amount += shortageOfMafias
+
+        DeckSettings.activeRoles = activeRoles
     }
 }
